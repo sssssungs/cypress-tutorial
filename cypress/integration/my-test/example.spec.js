8310fb5eadf1dc12902a3ea000426efa38d5a1c9
sssssungs <32057999+sssssungs@users.noreply.github.com>
2022-01-17 18:14:02 +0900

    added cypress example

---
+++ cypress/integration/my-test/example.spec.js
@@ -0,0 +1,8 @@
+/// <reference types="cypress" />
+
+describe('desc', () => {
+    it('test one', ()=>{
+        cy.visit('http://localhost:3000');
+        cy.contains('Edit src/App.tsx and save to reload.')
+    })
+})
