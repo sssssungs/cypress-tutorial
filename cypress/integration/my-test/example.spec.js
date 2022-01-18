152a138b59c499fbbdc617da1cac2df990f02171
sssssungs <32057999+sssssungs@users.noreply.github.com>
2022-01-18 20:08:20 +0900

    added beforeEach

--- cypress/integration/my-test/example.spec.js
+++ cypress/integration/my-test/example.spec.js
@@ -1,9 +1,12 @@
 /// <reference types="cypress" />
 
 describe("desc", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:3000");
+  });
+
   it("test: contain and should", () => {
     cy.viewport("iphone-se2");
-    cy.visit("http://localhost:3000");
     cy.contains("Edit src/App.tsx and save to reload.");
     cy.contains("reload.").should(
       "have.text",
@@ -12,18 +15,15 @@ describe("desc", () => {
   });
 
   it("test: click action", () => {
-    cy.visit("http://localhost:3000");
     cy.get("button").click();
     cy.contains("clicked!!");
   });
 
   it("test: get url", () => {
-    cy.visit("http://localhost:3000");
     cy.url().should("include", "localhost");
   });
 
   it("test: type some word", () => {
-    cy.visit("http://localhost:3000");
     cy.get("input").type("hello world");
   });
 });
